d0d8f20eff690a29a8975982b8168bc81c9bdf78
villabachris <villabachristopher@gmail.com>
2022-04-24 01:05:20 +0800

    adding validation

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -3,18 +3,15 @@ import {
   Get,
   Post,
   Query,
-  Param,
   Res,
   UseInterceptors,
   UploadedFile,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ReportsService } from './reports.service';
-import * as fs from 'fs';
-import * as path from 'path';
 import { diskStorage } from 'multer';
+import * as path from 'path';
 import { v4 as uuid } from 'uuid';
-import * as csv from 'csvtojson';
 import { Response } from 'express';
 
 @Controller()
@@ -23,52 +20,12 @@ export class ReportsController {
 
   @Get('/sales/report/')
   async dataRange(@Query() queryParams, @Res() res: Response): Promise<void> {
-    const csvFilePath = path.join(
-      process.cwd(),
-      '/files/Book1.bed8ff05-9019-4abe-81b9-89cb8584c93a.csv',
-    );
-    const jsonFile = await csv().fromFile(csvFilePath);
-    const dateRangeFilter = new Promise((resolve) => {
-      let filterData;
-      if (Object.keys(queryParams).length) {
-        try {
-          const startDate = queryParams.dateFrom
-            ? new Date(queryParams.dateFrom.toString())
-            : '';
-          const endDate = queryParams.dateTo
-            ? new Date(queryParams.dateTo.toString())
-            : '';
-          if (queryParams.dateFrom && queryParams.dateTo) {
-            filterData = jsonFile.filter((result) => {
-              const date = new Date(result.LAST_PURCHASE_DATE);
-              return date >= startDate && date <= endDate;
-            });
-            resolve(filterData);
-          }
-          if (queryParams.dateFrom) {
-            filterData = jsonFile.filter((result) => {
-              const date = new Date(result.LAST_PURCHASE_DATE);
-              return date >= startDate && date <= startDate;
-            });
-            resolve(filterData);
-          }
-          if (queryParams.dateTo) {
-            filterData = jsonFile.filter((result) => {
-              const date = new Date(result.LAST_PURCHASE_DATE);
-              return date >= endDate && date <= endDate;
-            });
-            resolve(filterData);
-          }
-        } catch (err) {
-          console.log(err);
-        }
-      } else {
-        console.log('dasdas');
-        filterData = jsonFile.map((data) => data);
-        resolve(filterData);
-      }
-    });
-    res.send(await dateRangeFilter);
+    return this.reportService.findReport(queryParams, res);
+  }
+
+  @Get('/files')
+  allFiles(@Res() res: Response) {
+    return this.reportService.allFiles(res);
   }
 
   @Post('/sales/record')
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,9 +1,88 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as csv from 'csvtojson';
 
 @Injectable()
 export class ReportsService {
   constructor() {}
-  async create(data_csv: any): Promise<object> {
+
+  allFiles(res): void {
+    const directoryPath = path.join(process.cwd(), `/files/`);
+    fs.readdir(directoryPath, (err, files) => {
+      //handling error
+      if (err) {
+        console.log('Unable to scan directory: ' + err);
+      }
+      res.send(files);
+    });
+  }
+
+  async findReport(queryParams, res): Promise<any> {
+    const directoryPath = path.join(process.cwd(), `/files/`);
+    const csvFilePath = `${directoryPath}${queryParams.csvFile}`;
+
+    if (!queryParams.csvFile) {
+      return res.send('Please provide a CSV file');
+    }
+
+    const jsonFile = await csv().fromFile(csvFilePath);
+
+    const dateRangeFilter = new Promise((resolve) => {
+      let filterData;
+      if (Object.keys(queryParams).length) {
+        try {
+          const startDate = queryParams.dateFrom
+            ? new Date(queryParams.dateFrom.toString())
+            : '';
+          const endDate = queryParams.dateTo
+            ? new Date(queryParams.dateTo.toString())
+            : '';
+          if (queryParams.dateFrom && queryParams.dateTo) {
+            if (startDate > endDate) {
+              resolve(`Date From cannot be later than Date to`);
+            } else if (endDate < startDate) {
+              resolve(`Date to cannot be earlier than Date From`);
+            }
+            filterData = jsonFile.filter((result) => {
+              const date = new Date(result.LAST_PURCHASE_DATE);
+              return date >= startDate && date <= endDate;
+            });
+            if (filterData.length === 0) {
+              resolve(
+                `No data at range between of ${queryParams.dateFrom} and ${queryParams.dateTo}`,
+              );
+            }
+            resolve(filterData);
+          }
+          if (queryParams.dateFrom) {
+            filterData = jsonFile.filter((result) => {
+              const date = new Date(result.LAST_PURCHASE_DATE);
+              return date >= startDate && date <= startDate;
+            });
+            resolve(filterData);
+          }
+          if (queryParams.dateTo) {
+            filterData = jsonFile.filter((result) => {
+              const date = new Date(result.LAST_PURCHASE_DATE);
+              return date >= endDate && date <= endDate;
+            });
+            resolve(filterData);
+          }
+        } catch (err) {
+          console.log('err', err);
+        }
+      } else {
+        filterData = jsonFile.map((data) => data);
+        resolve(filterData);
+      }
+    });
+    res.send(await dateRangeFilter);
+  }
+  async create(data_csv: any): Promise<any> {
+    if (typeof data_csv === 'undefined') {
+      return 'Please provide a CSV file';
+    }
     const response = {
       file: data_csv.originalname,
       size: data_csv.size,
